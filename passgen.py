99afae4084aa6100fbb6c82143e2d002a3c2752a
Luke Sheppard <lshep.usc@gmail.com>
2021-05-05 22:04:08 -0700

    Center random word. Allow 15.

--- passgen.py
+++ passgen.py
@@ -43,63 +43,55 @@ def get_memorable_password(size_of_password):
 
     random_word_of_proper_length = ''
 
-    if size_of_password < 20:
+    if size_of_password < 15:
         print("That won\'t work very well.")
-        print("You should have a password of 20 characters or more when using")
+        print("You should have a password of 15 characters or more when using")
         print("the random word feature.")
         exit()
 
     # Grab a random English word and its length
     random_word, random_word_length = get_random_word(wordlist, wordlist_length)
 
-    # if random_word_length <= int(size_of_password):
-    #
-    #     # We didn't need to modify the length so we just go ahead and rename it
-    #     random_word_of_proper_length = random_word
-    #
-    # else:
-
-    # The random word needs to be truncated
-
-    # The random word is too long for the password it will inhabit
     # Chop the random word down to about 50% as long as the password
     truncated_random_word_length = round(int(size_of_password) * 0.5)
 
-    # Randomly decide to chop off the beginning or the end of the word
-    # It will be mod 2 only 1/3 of the time. This way we will more often
-    # use the beginning of the word, that being easier to read.
-    if not random.randint(0, 2) % 2:
+    random_word_of_proper_length = random_word[:truncated_random_word_length]
 
-        # For a 10 character word, we want the first 7 characters only
-        random_word_of_proper_length = random_word[:truncated_random_word_length]
+    # Now surround the random word with random chars on each side
+    while len(random_word_of_proper_length) < int(password_size):
 
-    else:
+        random_word_of_proper_length = add_entropy_right(random_word_of_proper_length)
 
-        # For a 10 character word, we want the last 7 characters only
-        random_word_of_proper_length = random_word[truncated_random_word_length:]
+        # About half of the time, skip adding the left char. So we have more on the right
+        if random.randint(0,1) == 0:
+
+            pass
+
+        else:
+
+            random_word_of_proper_length = add_entropy_left(random_word_of_proper_length)
+
+    return random_word_of_proper_length
 
 
-    # Now we need to add entropy
 
-    if random.choice([0, 1]) == 0:
 
-        # Append random chars to the right
-        random_word_of_proper_length += ''.join([chr(random.randint(33, 126)) for i in range(0, int(password_size - len(random_word_of_proper_length)))])
 
-        return random_word_of_proper_length
+def add_entropy_right(rand_word):
 
-    else:
+    # Append one random char to the right
+    rand_word += ''.join(chr(random.randint(33, 126)))
 
-        # Prepend random chars to the left
-        random_string = ''.join([chr(random.randint(33, 126)) for i in
-                                    range(0, int(password_size - len(random_word_of_proper_length)))])
+    return rand_word
 
-        random_word_of_proper_length = (random_string + random_word_of_proper_length)
+def add_entropy_left(rand_word):
 
-        return random_word_of_proper_length
+    # Append one random char to the left
+    random_char = ''.join(chr(random.randint(33, 126)))
 
-    # TODO: Surround the random word with random chars on each side
+    rand_word = (random_char + rand_word)
 
+    return rand_word
 
 
 if __name__ == '__main__':
